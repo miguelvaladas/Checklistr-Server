1f5d0092d98f1f06f27dd69c3213f56c4b76cd24
airesvaladas <airesvaladas@gmail.com>
2022-09-15 19:15:57 +0100

    added tests

---
+++ BestCheckListAppEver-Server/BestCheckListEver/src/test/java/checklist/com/server/BestCheckListEver/java/Activity/ActivityServiceTest.java
@@ -0,0 +1,52 @@
+package checklist.com.server.BestCheckListEver.java.Activity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import checklist.com.server.BestCheckListEver.models.*;
+import checklist.com.server.BestCheckListEver.services.*;
+import checklist.com.server.BestCheckListEver.daos.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@ExtendWith(MockitoExtension.class)
+class ActivityServiceTest {
+
+	@InjectMocks
+	private ActivityServiceImpl underTest;
+
+	@Mock
+	private ActivityDao activityDao;
+
+	@Mock
+	private UserServiceImpl userService;
+
+	@Test
+	void canGetAllActivitiesFromUser() {
+		// given
+		Activity activity_1 = new Activity();
+		Activity activity_2 = new Activity();
+		Activity activity_3 = new Activity();
+		List<Activity> list = new ArrayList<>();
+		list.add(activity_1);
+		list.add(activity_2);
+		list.add(activity_3);
+
+		// when
+
+		when(activityDao.getAllUserActivities(anyString())).thenReturn(list);
+		when(userService.getByName(anyString())).thenReturn(new AppUser());
+		List<Activity> list_2 = underTest.getAllActivitiesFromUser("");
+
+		// then
+		assertEquals(list, list_2);
+		verify(activityDao, times(1)).getAllUserActivities(anyString());
+	}
+
+}
